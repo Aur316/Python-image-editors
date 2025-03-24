09747822e7cc4b95f9fb6d433114685af0a477d1
Aur316 <kristianfeher86@gmail.com>
2025-03-24 18:48:06 +0100

    new package

--- resize_images.py
+++ resize_images.py
@@ -1,17 +1,17 @@
 from PIL import Image
 import os
 
-folder_path = '/home/aur316/Letöltések/bck'
-output_folder = '/home/aur316/Letöltések/bck640'
+folder_path = '/Users/aur316/Pictures/bck'
+output_folder = '/Users/aur316/Pictures/bck64Blur'
 
 if not os.path.exists(output_folder):
-    os.makedirs(output_folder)  # Javítva: output_before helyett output_folder
+    os.makedirs(output_folder) 
 
 for filename in os.listdir(folder_path):
     if filename.endswith('.png'):
         img_path = os.path.join(folder_path, filename)
         img = Image.open(img_path)
-        img = img.resize((640, 640), Image.ANTIALIAS)
+        img = img.resize((640, 640), Image.Resampling.LANCZOS)
         img.save(os.path.join(output_folder, filename))
 
 print("All images have been resized and saved to", output_folder)
